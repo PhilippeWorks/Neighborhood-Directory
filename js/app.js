19c92940e1bfb38cc80dc59fd681fcbc95e53224
Philippe <philippe_t@live.ca>
2017-06-29 00:48:33 -0400

    Began implementing knockout.js ViewModel

--- js/app.js
+++ js/app.js
@@ -1,60 +1,36 @@
+function EverettLocation(name, address, phone, website, imgurl, position) {
+	var self = this;
+	self.name = name;
+	self.address = address;
+	self.website = website;
+	self.imgurl = imgurl;
+	self.position = position;
+};
+
+var ViewModel = function {
+	var self = this;
+
+	self.standard = {city: "Everett", province: "ON", postalcode: "L0M 1J0"}
+
+	self.places = ko.observableArray ([
+		new EverettLocation('Bailey\'s Potato Farm', '8159 Main Street Everett, Everett, ON L0M 1J0', '(705) 435-5764', '', {}),
+		new EverettLocation('RV There Yet Tent Trailer Rentals', '6218 County Rd 13, Everett, ON L0M 1J0', '(705) 434-9964', 'https://www.rvthereyettrailerrentals.com', '', ''),
+		new EverettLocation('Royal Canadian Legion Branch 508', '6190 County Rd 13, Everett, ON L0M 1J0', '705 435-5769', 'everettlegion.ca', '', ''),
+		new EverettLocation
+		]);
+
+
+
+};
+
+//ko.applyBindings(new ViewModel());
+
 var map;
 
-var model = {
-	currentPosition: null,
-	places: [
-		{
-			name: 'Bailey\'s Potato Farm', 
-			address: '8159 Main Street Everett, Everett, ON L0M 1J0', phone: '(705) 435-5764', 
-			img: '', 
-			position: {lat: 44.1899357, lng: -79.9460162}
-		},
-		{
-			name: 'RV There Yet Tent Trailer Rentals', 
-			address: '6218 County Rd 13, Everett, ON L0M 1J0', phone: '(705) 434-9964', 
-			website: 'https://www.rvthereyettrailerrentals.com', 
-			img: '', 
-			position: {lat: 44.1928181, lng: -79.944487}
-		},
-		{
-			name: 'Royal Canadian Legion Branch 508', 
-			address: '6190 County Rd 13, Everett, ON L0M 1J0', 
-			phone: '705 435-5769', 
-			website: 'everettlegion.ca', 
-			img: '', 
-			position: {lat: 44.1894557, lng: -79.9454385}
-		},
-		{
-			name: 'Bailey\'s Potato Farm' , 
-			phone: '705 435-5764', 
-			img: '', 
-			position: {lat: 44.1894557, lng: -79.9454385}
-		},
-		{
-			name: 'Bailey\'s Potato Farm', 
-			address: '8159 Main Street Everett, Everett, ON L0M 1J0', 
-			phone: '(705) 435-5764', 
-			img: '', 
-			position: {lat: 44.1899357, lng: -79.9460162}
-		},
-		{
-			name: 'Bailey\'s Potato Farm', 
-			address: '8159 Main Street Everett, Everett, ON L0M 1J0', 
-			phone: '(705) 435-5764', 
-			img: '', 
-			position: {lat: 44.1899357, lng: -79.9460162}
-		},
-		{
-			name: 'Bailey\'s Potato Farm', 
-			address: '8159 Main Street Everett, Everett, ON L0M 1J0', 
-			phone: '(705) 435-5764', 
-			img: '', 
-			position: {lat: 44.1899357, lng: -79.9460162}
-		}
-		]
-
-}
-var everett = {lat: 44.193342, lng: -79.934449};
+var everett = {
+	lat: 44.193342, 
+	lng: -79.934449
+};
 
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
@@ -65,4 +41,3 @@ function initMap() {
 		position: everett,
 		map: map
 	});
-}
