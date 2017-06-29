9d848b6ca132d5a870dcf445204721cbfcad7503
Philippe <philippe_t@live.ca>
2017-06-29 01:38:54 -0400

    Added all locations, hours and img's left

--- js/app.js
+++ js/app.js
@@ -10,14 +10,19 @@ function EverettLocation(name, address, phone, website, imgurl, position) {
 var ViewModel = function {
 	var self = this;
 
-	self.standard = {city: "Everett", province: "ON", postalcode: "L0M 1J0"}
+	self.standard = {city: "Everett", province: "ON", postalcode: "L0M 1J0"},
 
-	self.places = ko.observableArray ([
-		new EverettLocation('Bailey\'s Potato Farm', '8159 Main Street Everett, Everett, ON L0M 1J0', '(705) 435-5764', '', {}),
-		new EverettLocation('RV There Yet Tent Trailer Rentals', '6218 County Rd 13, Everett, ON L0M 1J0', '(705) 434-9964', 'https://www.rvthereyettrailerrentals.com', '', ''),
-		new EverettLocation('Royal Canadian Legion Branch 508', '6190 County Rd 13, Everett, ON L0M 1J0', '705 435-5769', 'everettlegion.ca', '', ''),
-		new EverettLocation
-		]);
+	self.places = ko.observableArray([
+		new EverettLocation('Bailey\'s Potato Farm', '8159 Main Street', '(705) 435-5764'),
+		new EverettLocation('RV There Yet Tent Trailer Rentals', '6218 County Rd 13', '(705) 434-9964', 'https://www.rvthereyettrailerrentals.com'),
+		new EverettLocation('Royal Canadian Legion Branch 508', '6190 County Rd 13', '705 435-5769', 'everettlegion.ca'),
+		new EverettLocation('Adjala-Tosorontio Township Fire Station 1', '6234 County Rd 13'),
+		new EverettLocation('Everett Variety & Snack', '8097 Main Street', '(705) 435-9616'),
+		new EverettLocation('Pizza Express', '8069 Main Street', '(705) 250-2016'),
+		new EverettLocation('MoneyGram', '8069 Main Street', '(705) 435-4537', 'moneygram.ca'),
+		new EverettLocation('Everett Post Office', '8069 Main Street', '(705) 435-4537'),
+		new EverettLocation('St David\'s Anglican Church and Cemetery', '6095 County Rd 13'),
+				]);
 
 
 
