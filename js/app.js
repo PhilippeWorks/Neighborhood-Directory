02b47678928f854903753af5cc170a5f1c8b7607
Philippe <philippe_t@live.ca>
2017-07-02 05:00:51 -0400

    add style experiementations and a makeMarkerIcon function

--- js/app.js
+++ js/app.js
@@ -2,6 +2,72 @@ var map;
 var markers = [];
 
 function initMap() {
+	        var styles = [
+          {
+            featureType: 'water',
+            stylers: [
+              { color: '#19a0d8' }
+            ]
+          },{
+            featureType: 'administrative',
+            elementType: 'labels.text.stroke',
+            stylers: [
+              { color: '#ffffff' },
+              { weight: 6 }
+            ]
+          },{
+            featureType: 'administrative',
+            elementType: 'labels.text.fill',
+            stylers: [
+              { color: '#e85113' }
+            ]
+          },{
+            featureType: 'road.highway',
+            elementType: 'geometry.stroke',
+            stylers: [
+              { color: '#efe9e4' },
+              { lightness: -40 }
+            ]
+          },{
+            featureType: 'transit.station',
+            stylers: [
+              { weight: 9 },
+              { hue: '#e85113' }
+            ]
+          },{
+            featureType: 'road.highway',
+            elementType: 'labels.icon',
+            stylers: [
+              { visibility: 'off' }
+            ]
+          },{
+            featureType: 'water',
+            elementType: 'labels.text.stroke',
+            stylers: [
+              { lightness: 100 }
+            ]
+          },{
+            featureType: 'water',
+            elementType: 'labels.text.fill',
+            stylers: [
+              { lightness: -100 }
+            ]
+          },{
+            featureType: 'poi',
+            elementType: 'geometry',
+            stylers: [
+              { visibility: 'on' },
+              { color: '#f0e4d3' }
+            ]
+          },{
+            featureType: 'road.highway',
+            elementType: 'geometry.fill',
+            stylers: [
+              { color: '#efe9e4' },
+              { lightness: -25 }
+            ]
+          }
+        ];
 
 	var everett = {
 		lat: 44.193342, 
@@ -10,7 +76,9 @@ function initMap() {
 
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: everett,
-		zoom: 15
+		zoom: 15,
+		styles: styles,
+		mapTypeControl: false
 	});
 
 	var locations = [
@@ -26,30 +94,43 @@ function initMap() {
 			];
 
 	var allInfoWindow = new google.maps.InfoWindow();
-	var bounds = new google.maps.LatLngBounds();
+
+	var defaultIcon = makeMarkerIcon('0091ff');
+
+	var highlightedIcon = makeMarkerIcon('FFFF24');
+
 
 	for(var i = 0; i < locations.length; i++) {
 		var position = locations[i].position;
 		var title = locations[i].title;
 
 		var marker = new google.maps.Marker({
-			map: map,
 			position: position,
 			title: title,
+			icon: defaultIcon,
 			animation: google.maps.Animation.DROP,
 			id: i
 		});
 
 		markers.push(marker);
 
-		bounds.extend(markers[i].position);
-
 		marker.addListener('click', function(){
 			populateInfoWindow(this, allInfoWindow)
 		});
+
+		marker.addListener('mouseover', function() {
+			this.setIcon(highlightedIcon);
+		});
+
+		marker.addListener('mouseout', function() {
+			this.setIcon(defaultIcon);
+		});
 	};
 
-	map.fitBounds(bounds);
+	document.getElementById('show-locations').addEventListener('click', showListings);
+	document.getElementById('hide-locations').addEventListener('click', hideListings);
+
+	};
 
 	function populateInfoWindow(marker, infowindow) {
 		if (infowindow.marker != marker) {
@@ -58,10 +139,40 @@ function initMap() {
 			infowindow.open(map, marker);
 
 			infowindow.addListener('closeclick', function() {
-				infowindow.setMarker(null);
+				infowindow.marker = null;
 			});
 		}
 	}
+
+	      // This function will loop through the markers array and display them all.
+      function showListings() {
+        var bounds = new google.maps.LatLngBounds();
+        // Extend the boundaries of the map for each marker and display the marker
+        for (var i = 0; i < markers.length; i++) {
+          markers[i].setMap(map);
+          bounds.extend(markers[i].position);
+        }
+        map.fitBounds(bounds);
+      }
+      // This function will loop through the listings and hide them all.
+      function hideListings() {
+        for (var i = 0; i < markers.length; i++) {
+          markers[i].setMap(null);
+        }
+      };
+
+            function makeMarkerIcon(markerColor) {
+        var markerImage = new google.maps.MarkerImage(
+          'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+          '|40|_|%E2%80%A2',
+          new google.maps.Size(21, 34),
+          new google.maps.Point(0, 0),
+          new google.maps.Point(10, 34),
+          new google.maps.Size(21,34));
+        return markerImage;
+      }
+ 
+
 
 	// //single marker and infowindow implementation
 	// var marker = new google.maps.Marker({
@@ -70,14 +181,13 @@ function initMap() {
 	// 	title: 'Everett!'
 	// })
 
-	// var infowindow = new google.maps.InfoWindow({
-	// 	content: 'This is Everett'
-	// });
-	// marker.addListener('click', function() {
-	// 	infowindow.open(map, marker);
-	// });
+// var infowindow = new google.maps.InfoWindow({
+// 	content: 'This is Everett'
+// });
+// marker.addListener('click', function() {
+// 	infowindow.open(map, marker);
+// });
 
-};
 
 
 
